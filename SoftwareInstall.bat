a24e4fe1dd17d3ef2cde1a7d8f9f8dcc694d77bf
Father Fox <105367617+awfullyavg@users.noreply.github.com>
2022-05-11 10:55:14 -0400

    Add files via upload

---
+++ SoftwareInstall.bat
@@ -0,0 +1,65 @@
+::This program was intended to help automate the installation of softwares located on a local server.
+::This portion of the program executes the installations of the called softwares.
+::Ninja
+Start "" "T:\IT - Software\Ninja\workshopapdmainoffice-4.0.5130-windows-installer.msi"
+:exit
+@pause
+::
+::Google Chrome
+Start "" "T:\IT - Software\Browser\ChromeSetup.exe"
+:exit
+@pause
+::
+::FireFox
+Start "" "T:\IT - Software\Browser\Firefox Installer.exe"
+:exit
+@pause
+::
+::MS Office
+Start "" "T:\IT - Software\MS Office\OfficeSetup.exe"
+:exit
+@pause
+::
+::MS Teams
+Start "" "T:\IT - Software\MS Teams\Teams_windows_x64.exe"
+:exit
+@pause
+::
+::ACAD 2019
+Start "" "T:\IT - Software\AutoCAD 2019\ACAD 2019.lnk"
+:exit
+@pause
+::Revit 2019
+Start "" "T:\IT - Software\Revit 2019\AutodeskDownloadManager_webinstall.exe"
+:exit
+@pause
+::Revit 2020
+Start "" "T:\IT - Software\Revit 2020\Revit 2020.lnk"
+:exit
+@pause
+::
+::SketchUp
+Start "" "T:\IT - Software\Sketchup Pro 2019\SketchUpPro-2019-en"
+:exit
+@pause
+::
+::Adobe
+Start "" "T:\IT - Software\Adobe CC\Design-Test_en_US_WIN_64\Design-Test\Build\Design-Test.msi"
+:exit
+@pause
+::
+::BlueBeam
+Start "" "T:\IT - Software\BlueBeam\BbRevu2019.1.20.exe"
+:exit
+@pause
+::
+::Rhino 7
+Start "" "T:\IT - Software\Rhino 7\rhino_en-us_7.2.21012.11001.exe"
+:exit
+@pause
+::
+::Rhino 6
+Start "" "T:\IT - Software\Rhino 6\rhino_en-us_6.18.19266.14201.exe"
+:exit
+@pause
+::
